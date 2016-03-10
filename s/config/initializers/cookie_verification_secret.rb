ba9b00752c4af2b5c1a10046164dd3fabf4b159d
satyam pawar <satyampawar007@gmail.com>
2016-03-11 01:38:29 +0530

    push afriends socialsite

--- s/config/initializers/cookie_verification_secret.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.cookie_verifier_secret = 'f90bfbb11add962c4d655f82ac10207ffee81309ff2e12452eff66fb1fb6b9a0f5889560744e257a724bef4156ad8e25bddcdc96135a64f73dd45a04d0eac0b8';
