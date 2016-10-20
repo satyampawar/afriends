9b2f4b693708948dd6140d3f5962bd5d700eaca5
Hitesh Kanwar <hitesh.kanwar786@gmail.com>
2016-10-20 17:37:38 +0530

    next

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ config.action_mailer.smtp_settings = {
 
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
-
+  config.assets.precompile += %w( private_pub.js )
   # Use a different logger for distributed setups.
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
 
