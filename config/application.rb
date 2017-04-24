89ff51099de42454ba6b605184a47bbccd14e3bb
satyam <satyamyuvasoft158@gmail.com>
2017-04-24 15:43:12 +0530

    feature fixed

--- config/application.rb
+++ config/application.rb
@@ -39,16 +39,6 @@ module Picssi
     # Precompile *all* assets, except those that start with underscore
    config.assets.precompile << /(^[^_\/]|\/[^_])[^\/]*$/
   end
-    ActionDispatch::Callbacks.after do      
-      # Reload the factories
-      return unless (Rails.env.development? || Rails.env.test?)
-      
-      unless FactoryGirl.factories.blank? # first init will load factories, this should only run on subsequent reloads
-        FactoryGirl.factories.clear
-        FactoryGirl.find_definitions
-      end
-
-    end    
 end
 
 
