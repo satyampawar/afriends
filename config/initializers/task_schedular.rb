aff10d20e33c23cdff07ec7c468f55bf274be45f
Satyam Pawar <satyamyuvasoft158@gmail.com>
2017-05-02 00:18:00 +0530

    feature fix

--- config/initializers/task_schedular.rb
+++ config/initializers/task_schedular.rb
@@ -2,7 +2,7 @@ require 'rufus-scheduler'
 
 scheduler = Rufus::Scheduler.new
 
-scheduler.in '49s' do
-  @notification = NotificationsController.new
-  @notification.birthday_notification
-end
+# scheduler.in '49s' do
+#   @notification = NotificationsController.new
+#   @notification.birthday_notification
+# end
